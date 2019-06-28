855d168e4d3236545bc70cc9a2e83878eda6cb3f
Ayman-Hunjul <aymanhun@gmail.com>
2019-06-28 22:14:35 +0300

    Update Login2Controller.java

--- src/Login2/Login2Controller.java
+++ src/Login2/Login2Controller.java
@@ -12,18 +12,17 @@ import java.util.ResourceBundle;
 public class Login2Controller implements Initializable {
 
     @FXML
-    private MediaView mediaViewsss;
+    private MediaView mediaView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         Media media=new Media("http://94.130.11.106/dl/s10/v6ImQhrLbTRoiFpzw3l8_A,1533924438/yt:ugTaLEBu6g8-8/A%20short%20Meditation%20of%20Nature%20Sounds-Relaxing%20Birdsong-Calming%20Sound%20of%20Water%20Relaxation.mp4");
         MediaPlayer player=new MediaPlayer(media);
-        mediaViewsss.setMediaPlayer(player);
+        mediaView.setMediaPlayer(player);
         player.setVolume(0);
         player.play();
 
     }
 }
-
 
